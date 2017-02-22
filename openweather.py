e32c20cfa87acdef48949997998a3fb918ca02a4
Manuel Buil <mbuil@suse.com>
2017-02-22 15:53:43 +0000

    Added logging and some logic when humidity is very low
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- openweather.py
+++ openweather.py
@@ -1,9 +1,11 @@
-import pyowm
+import pyowm #How to use it:
 
 owm = pyowm.OWM('252115bbe068ef499054670c9494249b')  # You MUST provide a valid API key
+town = "Sabinanigo,es"
+
 
 def _get_forecast_tomorrow():
-    return owm.daily_forecast("Sabinanigo,es", limit=1)
+    return owm.daily_forecast(town, limit=1)
     
 def rain_tomorrow():
     forecast = _get_forecast_tomorrow()
@@ -23,3 +25,10 @@ def humidity_tomorrow():
     humidity = weathers[0].get_humidity()    
     return humidity
 
+def rain_today():
+    obs = owm.weather_at_place(town)
+    weather = obs.get_weather()
+    if len(weather.get_rain()) > 0:
+        return True
+    else:
+        return False
--- watering_check.py
+++ watering_check.py
@@ -1,18 +1,34 @@
 import sensor
 import openweather as ow
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 def main():
 
+    ow.rain_today()
+
+
     # Check the humidity
     hum = sensor.read_humidity()
+    logger.info("This is the humidity: %s" %hum)
 
     if hum < 60:
+        logger.info("Will it rain tomorrow? %s" %ow.rain_tomorrow())
+        logger.info("Max. temperature tomorrow: %s" %ow.maxT_tomorrow())
         if (not ow.rain_tomorrow() and ow.maxT_tomorrow() > 25):
             # We need to water the plant
             print("We must water the plants")
             return
-            
-    print("Nothing to do")
+           
+    if hum < 35:
+        if (not ow.rain_today()):
+           print("Water the plants a bit")
+           return
+
+    logger.info("Nothing to do today")
 
 
 if __name__ == "__main__":
